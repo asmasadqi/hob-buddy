4925e0a183aba626a2e0a3cf1962c08ce455292d
ALexis Brun <alexisbn@outlook.com>
2023-02-25 12:22:26 +0100

    s

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
       preferences_and_location = User.where.not(id: current_user.id).near(current_user.location, 100) #modifier la seed pour faire matcher les gens
       first_batch_users = preferences_and_location.where(user_preferences: UserPreference.where(preference_id: preferences))
       second_batch_users = preferences_and_location.where.not(id: first_batch_users.map(&:id))
-      @users = first_batch_users + second_batch_users
+      @users = (first_batch_users + second_batch_users).uniq
     else
       @users = User.all
     end

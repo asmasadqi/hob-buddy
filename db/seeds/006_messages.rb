f1458327d9da3a7ecf6dd1173f6727c3a6973ac3
Kaori Martin <kaori.martin21@gmail.com>
2023-03-02 23:15:32 +0100

    Britney photo edited

--- db/seeds/006_messages.rb
+++ db/seeds/006_messages.rb
@@ -12,29 +12,29 @@ Chatroom.all.each do |chatroom|
   end
 end
 m1 = Message.create(
-  content: "Hello everyone i hope yall are excited to learn Diving",
+  content: "Hello Ayla :)",
   user_id: User.find_by(email: "kao_martin@gmail.com").id,
   chatroom_id: chatroom_id
 )
 puts "message created with id#{m1.id} in chatroom #{chatroom_id}"
 
 m2 = Message.create(
-  content: "Hiiii my name is Ayla, nice to meet you!",
+  content: "Hiiii, nice to meet you! How are you?",
   user_id: User.find_by(email: "ayla.moon@gmail.com").id,
   chatroom_id: chatroom_id
 )
 puts "message created with id#{m2.id} in chatroom #{chatroom_id}"
 
 m3 = Message.create(
-  content: "Hello, I'm Victoria! You can call me Vic :) How are you doing?",
-  user_id: User.find_by(email: "victoria.semblat@gmail.com").id,
+  content: "Hey! I'm doing good :) What kind of activities do you want to do?",
+  user_id: User.find_by(email: "kao_martin@gmail.com").id,
   chatroom_id: chatroom_id
 )
 puts "message created with id#{m3.id} in chatroom #{chatroom_id}"
 
 m4 = Message.create(
-  content: "Hey! I'm doing good :) Are you free next week?",
-  user_id: User.find_by(email: "kao_martin@gmail.com").id,
+  content: "Bouldering, surfing...let's talk about it when we meet ! I would love to get to know you!! Are you free next week for a coffee?",
+  user_id: User.find_by(email: "ayla.moon@gmail.com").id,
   chatroom_id: chatroom_id
 )
 puts "message created with id#{m4.id} in chatroom #{chatroom_id}"
